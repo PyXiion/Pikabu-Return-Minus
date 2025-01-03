ee07021cd41a7819751f92ddd809106d7d10e7d2
PyXiion <me@pyxiion.ru>
2025-01-03 22:34:43 +0300

    0.9.6

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Return Pikabu minus
-// @version      0.9.5
+// @version      0.9.6
 // @namespace    pikabu-return-minus.pyxiion.ru
 // @description  Возвращает минусы на Pikabu, а также фильтрацию по рейтингу.
 // @author       PyXiion
